4105cca3885c5916b31cf1f0961bf9a979d2bd3f
Jiwon <hajw.study@gmail.com>
2024-01-15 21:08:54 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,23 @@ export default function Home() {
     <main>
       내가 메인
       <div>
-        <Button size="small" mode={"success"}>
+        <Button size='small' mode={"success"}>
           나는 스몰
         </Button>
-        <Button size="medium" mode={"primary"}>
+        <Button size='medium' mode={"primary"}>
           나는 미디움
         </Button>
-        <Button size="large" mode={"error"}>
+        <Button size='large' mode={"error"}>
           나는 라지
+        </Button>
+
+        <Button textType={"text_primary"} mode={"none"}>
+          글자 버튼
         </Button>
 
         <Button
           onClick={() => setVisible(true)}
-          size="large"
+          size='large'
           mode={"error"}
           style={{ fontSize: 16 }}
         >
@@ -32,11 +36,15 @@ export default function Home() {
         </Button>
       </div>
       <div>
-        <Input size="small" mode={"success"} />
-        <Input size="medium" mode={"primary"} />
-        <Input size="large" mode={"error"} />
+        <Input size='small' mode={"success"} />
+        <Input size='medium' mode={"primary"} />
+        <Input size='large' mode={"error"} />
       </div>
-      <Modal onClose={() => setVisible(false)} style={{ width: 300, height: 200 }} visible={visible}>
+      <Modal
+        onClose={() => setVisible(false)}
+        style={{ width: 300, height: 200 }}
+        visible={visible}
+      >
         내가 모달이야!
       </Modal>
     </main>
