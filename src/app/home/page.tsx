451f8d2aa05288672968c155e700dd248af6cd99
myeonghwa <myeonghwa0116@gmail.com>
2024-02-01 23:56:34 +0900

    Add: 사이드바 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,43 +1,156 @@
-import Card from '@/component/Card/Card'
-import PositionSquare from '@/component/PositionSquare/PositionSquare'
+"use client"
+
+import styled from "@emotion/styled";
+import Card from "@/component/Card/Card";
+import Input from "@/component/Input/Input";
+import PositionSquare from "@/component/PositionSquare/PositionSquare";
+import Button from "@/component/Button/Button";
 
 const data = [
   {
     id: 1,
     title: "프로젝트 1",
     startDate: "2024-02-01",
-    endDate: "2024-02-20",
+    endDate: "2024-02-28",
     deposit: "10만원",
     positions: ["디자이너", "기획자", "프론트엔드", "백엔드"],
   },
   {
     id: 2,
     title: "프로젝트 2",
-    startDate: "2024-03-01",
+    startDate: "2024-02-01",
+    endDate: "2024-02-15",
+    deposit: "15만원",
+    positions: ["디자이너", "백엔드"],
+  },
+  {
+    id: 3,
+    title: "프로젝트 3",
+    startDate: "2024-02-05",
+    endDate: "2024-02-25",
+    deposit: "20만원",
+    positions: ["프론트엔드", "백엔드"],
+  },
+  {
+    id: 4,
+    title: "프로젝트 4",
+    startDate: "2024-02-10",
+    endDate: "2024-03-30",
+    deposit: "25만원",
+    positions: ["디자이너", "기획자"],
+  },
+  {
+    id: 5,
+    title: "프로젝트 5",
+    startDate: "2024-03-15",
+    endDate: "2024-03-25",
+    deposit: "30만원",
+    positions: ["백엔드"],
+  },
+  {
+    id: 6,
+    title: "프로젝트 6",
+    startDate: "2024-02-20",
+    endDate: "2024-03-10",
+    deposit: "20만원",
+    positions: ["프론트엔드"],
+  },
+  {
+    id: 7,
+    title: "프로젝트 7",
+    startDate: "2024-02-25",
     endDate: "2024-03-15",
     deposit: "15만원",
     positions: ["디자이너", "백엔드"],
+  },
+  {
+    id: 8,
+    title: "프로젝트 8",
+    startDate: "2024-03-30",
+    endDate: "2024-04-20",
+    deposit: "25만원",
+    positions: ["기획자", "프론트엔드"],
+  },
+  {
+    id: 9,
+    title: "프로젝트 9",
+    startDate: "2024-03-25",
+    endDate: "2024-04-05",
+    deposit: "30만원",
+    positions: ["디자이너", "백엔드"],
+  },
+  {
+    id: 10,
+    title: "프로젝트 10",
+    startDate: "2024-04-10",
+    endDate: "2024-04-30",
+    deposit: "20만원",
+    positions: ["프론트엔드"],
   },
 ];
 
 const page = () => {
   return (
-    <>
-      {data.map((project) => (
-        <Card
-          key={project.id}
-          title={project.title}
-          startDate={project.startDate}
-          endDate={project.endDate}
-          deposit={project.deposit}
-        >
-          {project.positions.map((position, index) => (
-            <PositionSquare key={index} value={position} />
-          ))}
-        </Card>
-      ))}
-    </>
+    <div>
+      <Title>사이드 플젝</Title>
+      <Wrap>
+        <ButtonWrap>
+          <Button>전체</Button>
+          <Button>디자인</Button>
+          <Button>기획</Button>
+          <Button>개발</Button>
+        </ButtonWrap>
+        <Input
+          type="search"
+          name="input"
+          placeholder="프로젝트 검색"
+          size="large"
+        />
+      </Wrap>
+      <CardWrap>
+        {data.map((project) => (
+          <Card
+            key={project.id}
+            title={project.title}
+            startDate={project.startDate}
+            endDate={project.endDate}
+            deposit={project.deposit}
+          >
+            {project.positions.map((position, index) => (
+              <PositionSquare key={index} value={position} />
+            ))}
+          </Card>
+        ))}
+      </CardWrap>
+    </div>
   );
-}
+};
 
 export default page;
+
+const Title = styled.div`
+  color: var(--txt-main, #1e1e20);
+  font-family: Pretendard;
+  font-size: 28px;
+  font-weight: 800;
+  letter-spacing: -1.12px;
+`;
+
+const Wrap = styled.div`
+  margin-top: 21px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ButtonWrap = styled.div`
+  display: inline-flex;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+const CardWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px 18px;
+  margin-top: 24px;
+`;
---
+++ src/component/Navigation/Navigation.tsx
@@ -0,0 +1,255 @@
+"use client";
+
+import styled from "@emotion/styled";
+import Image from "next/image";
+import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
+import Button from "../Button/Button";
+
+const Navigation = () => {
+  const segment = useSelectedLayoutSegment();
+
+  return (
+    <Container>
+      <Link href="/home">
+        <Image src="images/logo.svg" alt="logo" width={60} height={33.623} />
+      </Link>
+      <NavWrap>
+        <MenuWrap>
+          <Link href="/home" style={{ textDecoration: "none" }}>
+            <Menu>
+              <MenuDetail>
+                <Image
+                  src={
+                    segment === "home"
+                      ? "images/home/home_blue.svg"
+                      : "images/home/home_gray.svg"
+                  }
+                  alt="home"
+                  width={20}
+                  height={20}
+                />
+                Home
+              </MenuDetail>
+              {segment === "home" && (
+                <Image
+                  src="images/home/more.svg"
+                  alt="more"
+                  width={24}
+                  height={24}
+                />
+              )}
+            </Menu>
+          </Link>
+          <Link href="/myPage" style={{ textDecoration: "none" }}>
+            <Menu>
+              <MenuDetail>
+                <Image
+                  src={
+                    segment === "myPage"
+                      ? "images/home/calendar_blue.svg"
+                      : "images/home/calendar_gray.svg"
+                  }
+                  alt="myPage"
+                  width={20}
+                  height={20}
+                />
+                마이페이지
+              </MenuDetail>
+              {segment === "myPage" && (
+                <Image
+                  src="images/home/more.svg"
+                  alt="more"
+                  width={24}
+                  height={24}
+                />
+              )}
+            </Menu>
+          </Link>
+          <Link href="/resume" style={{ textDecoration: "none" }}>
+            <Menu>
+              <MenuDetail>
+                <Image
+                  src={
+                    segment === "resume"
+                      ? "images/home/setup_blue.svg"
+                      : "images/home/setup_gray.svg"
+                  }
+                  alt="resume"
+                  width={20}
+                  height={20}
+                />
+                지원서 관리
+              </MenuDetail>
+              {segment === "resume" && (
+                <Image
+                  src="images/home/more.svg"
+                  alt="more"
+                  width={24}
+                  height={24}
+                />
+              )}
+            </Menu>
+          </Link>
+          <Link href="/payment" style={{ textDecoration: "none" }}>
+            <Menu>
+              <MenuDetail>
+                <Image
+                  src={
+                    segment === "payment"
+                      ? "images/home/calendar_blue.svg"
+                      : "images/home/calendar_gray.svg"
+                  }
+                  alt="payment"
+                  width={20}
+                  height={20}
+                />
+                결제 내역
+              </MenuDetail>
+              {segment === "payment" && (
+                <Image
+                  src="images/home/more.svg"
+                  alt="more"
+                  width={24}
+                  height={24}
+                />
+              )}
+            </Menu>
+          </Link>
+          <Link href="/project" style={{ textDecoration: "none" }}>
+            <Menu>
+              <MenuDetail>
+                <Image
+                  src={
+                    segment === "project"
+                      ? "images/home/bookmark_blue.svg"
+                      : "images/home/bookmark_gray.svg"
+                  }
+                  alt="project"
+                  width={20}
+                  height={20}
+                />
+                프로젝트
+              </MenuDetail>
+              {segment === "project" && (
+                <Image
+                  src="images/home/more.svg"
+                  alt="more"
+                  width={24}
+                  height={24}
+                />
+              )}
+            </Menu>
+          </Link>
+        </MenuWrap>
+        <ProfileWrap>
+          <Image src="images/home/profile.svg" alt="profile" width={94} height={94} />
+          <Profile>
+            <Name>808Ground</Name>
+            <Job>
+              <JobDetail>10년차</JobDetail>
+              <JobDetail>디자이너</JobDetail>
+            </Job>
+          </Profile>
+          <Button mode="primary">마이페이지</Button>
+        </ProfileWrap>
+      </NavWrap>
+    </Container>
+  );
+};
+
+export default Navigation;
+
+const Container = styled.div`
+  display: inline-flex;
+  box-sizing: border-box;
+  min-height: 100vh;
+  padding: 80px 50px;
+  flex-direction: column;
+  align-items: center;
+  gap: 100px;
+  flex-shrink: 0;
+  justify-content: flex-start;
+  position: fixed;
+
+  border-radius: 14px 30px 50px 30px;
+  background: var(--White, #fff);
+`;
+
+const NavWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1 0 0;
+`;
+
+const MenuWrap = styled.div`
+  display: flex;
+  width: 146px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 30px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  width: 146px;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+
+const MenuDetail = styled.div`
+  color: #0066ff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  flex: 1 0 0;
+`;
+
+const ProfileWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Name = styled.div`
+  color: var(--primary, #06f);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const Job = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 7px;
+`;
+
+const JobDetail = styled.div`
+  color: var(--primary, #06f);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
