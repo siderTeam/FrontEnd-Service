52b7fae1d22d97acceb32fbd2ff1ed2961894e34
MyeonghwaSon <myeonghwa0116@gmail.com>
2024-02-09 19:41:47 +0900

    Add: select box css 수정

--- src/component/SelectBox/SelectBox.tsx
+++ src/component/SelectBox/SelectBox.tsx
@@ -2,7 +2,6 @@
 
 import styled from "@emotion/styled";
 import { useState } from "react";
-
 import { SelectBoxProps } from "@/type/types";
 
 const SelectBox = ({
@@ -26,10 +25,14 @@ const SelectBox = ({
     setVisible(false);
   };
 
-  const selected = options.filter((option) => option.value === value);
+  const handleBlur = () => {
+    setVisible(false);
+  };
 
+  const selected = options.filter((option) => option.value === value);
+  
   return (
-    <Container>
+    <Container tabIndex={0} onBlur={handleBlur}>
       <StyledSelect
         size={size}
         value={value}
@@ -37,7 +40,9 @@ const SelectBox = ({
         style={style}
         onClick={handleClickSelect}
       >
-        <div className="value">{selected.length === 0 ? placeholder : selected[0].label}</div>
+        <div className="value">
+          {selected.length === 0 ? placeholder : selected[0].label}
+        </div>
       </StyledSelect>
       {visible && (
         <OptionWrapper size={size}>
@@ -61,6 +66,7 @@ export default SelectBox;
 
 const Container = styled.div`
   position: relative;
+  cursor: pointer;
 `;
 
 const SELECT_TYPE = {
@@ -78,13 +84,57 @@ const SELECT_TYPE = {
   },
 };
 
+const StyledSelect = styled.div<any>`
+  //${({ size }) => SELECT_TYPE[size as "medium"]};
+  width: 100%;
+  height: 52px;
+  font-size: 16px;
+  border: 1px solid #b8b8b8;
+  border-radius: 6px;
+  outline: none;
+  padding: 0 10px;
+  box-sizing: border-box;
+  background: white;
+
+  &::before {
+    content: "⌵";
+    position: absolute;
+    top: 15px;
+    right: 20px;
+    font-size: 20px;
+  }
+
+  .value {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const OptionWrapper = styled.ul<{ size: "small" | "medium" | "large" }>`
   position: absolute;
+  width: 100%;
   font-size: 16px;
   box-sizing: border-box;
   top: ${({ size }) => SELECT_TYPE[size].height};
   background: white;
   z-index: 3;
+
+  max-height: 200px;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 10px; /* 스크롤바의 너비 */
+  }
+  &::-webkit-scrollbar-thumb {
+    height: 30%; /* 스크롤바의 길이 */
+    background: #848484; /* 스크롤바의 색상 */
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background: #84848447; /*스크롤바 뒷 배경 색상*/
+  }
 
   li {
     display: flex;
@@ -100,25 +150,5 @@ const OptionWrapper = styled.ul<{ size: "small" | "medium" | "large" }>`
 
   li:hover {
     background: #eeeeee;
-  }
-`;
-
-const StyledSelect = styled.div<any>`
-  //${({ size }) => SELECT_TYPE[size as "medium"]};
-  width: 100%;
-  height: 52px;
-  font-size: 16px;
-  border: 1px solid #b8b8b8;
-  border-radius: 6px;
-  outline: none;
-  padding: 0 10px;
-  box-sizing: border-box;
-  background: white;
-
-  .value {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
   }
 `;
