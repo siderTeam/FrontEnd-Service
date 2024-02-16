4ab45beaeac75c3cf7d51452380e25fad1d5b2b6
Jiwon <hajw.study@gmail.com>
2024-02-16 18:18:48 +0900

    컬러 컴포넌트

---
+++ src/component/Styles/CommonStyles.tsx
@@ -0,0 +1,15 @@
+export const color = {
+  brandMain: "#01FF1A",
+
+  black: "#02060D",
+  gray9: "#1D2939",
+  gray8: "#344054",
+  gray7: "#667085",
+  gray6: "#98A2B3",
+  gray5: "#D0D5DD",
+  gray4: "#EAECF0",
+  gray3: "#F2F4F7",
+  gray2: "#F9FAFB",
+  gray1: "#FCFCFD",
+  white: "#FFFFFF",
+};
