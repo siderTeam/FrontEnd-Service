d598e2d830992c4d54a19eb8885a158dc0296b5d
Jiwon <hajw.study@gmail.com>
2024-03-02 17:57:20 +0900

    사이드바 수정

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,160 +2,180 @@
 
 import styled from '@emotion/styled';
 import { color } from '@/styles/color';
-import Link from 'next/link';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const router = [
   {
-    path: '/admin/dashBoard',
+    path: '/dashBoard',
     label: '대시보드',
     iconPath: 'home',
     activeIconPath: 'home_green',
   },
   {
-    path: '/admin/siteManage',
+    path: '/siteManage',
     label: '사이트 관리',
     iconPath: 'setting',
     activeIconPath: 'setting_green',
   },
   {
-    path: '/admin/memberManage',
-    label: '회원 관리',
+    path: '/memberManage',
+    label: '회원관리',
     iconPath: 'people',
-    activeIconPath: 'people_green',
-    subMenu: [{ path: '/admin/memberManage/memberList', label: '회원 목록' }],
+    activeIconPath: '/people_green',
+    subMenu: [
+      {
+        path: '/memberList',
+        label: '회원 목록',
+      },
+    ],
+    height: 20,
   },
   {
-    path: '/admin/projectManage',
-    label: '프로젝트',
+    path: '/projectManage',
+    label: '프로젝트 관리',
     iconPath: 'monitor',
     activeIconPath: 'monitor_green',
     subMenu: [
-      { path: '/admin/projectManage/requireJudge', label: '요구사항 심사' },
-      { path: '/admin/projectManage/depositJudge', label: '보증금 심사' },
-      { path: '/admin/projectManage/projectJudge', label: '프로젝트 준공 심사' },
+      {
+        path: '/requireJudge',
+        label: '요구사항 심사',
+      },
+      {
+        path: '/depositJudge',
+        label: '보증금 심사',
+      },
+      {
+        path: '/projectCompletion',
+        label: '프로젝트 준공 심사',
+      },
     ],
-    height: 62,
+    height: 100,
   },
 ];
 
-const Sidebar = () => {
+const AdminSidebar = () => {
   const path = usePathname();
-  const [arcodian, setArcodian] = useState(false);
-  const [activeMenu, setActiveMenu] = useState('');
+  const [activeMenu, setActiveMenu] = useState(path);
+  const [openMenu, setOpenMenu] = useState(path);
 
-  const handleClick = (path: string) => {
-    if (activeMenu === path) {
-      setArcodian(!arcodian);
-    } else {
-      setArcodian(true);
-    }
+  const handleClickMenu = (path: string) => {
     setActiveMenu(path);
+    setOpenMenu('');
   };
 
-  const handleClickOther = () => {
-    setArcodian(false);
+  const handleClickOpen = (path: string) => {
+    setOpenMenu(path);
   };
-
-  console.log(arcodian);
 
   return (
-    <SidebarContainer>
-      <div className="top">
-        <Image className="logo" src="/images/logo.svg" alt="logo" width={133} height={46} />
+    <Container>
+      <div className="wrap">
+        <Link href={'/admin/dashboard'}>
+          <Image className="logo" src={'/images/logo.svg'} alt="logo" width={133} height={46} />
+        </Link>
 
-        <RouteWrapper>
-          {router.map((route) => (
-            <div className={route.subMenu && activeMenu === route.path && arcodian ? 'route_box' : 'route_box_nosub'} key={route.path}>
-              <div className="wrap">
-                {!route.subMenu && (
-                  <>
-                    <Image
-                      src={path === route.path ? `/images/etc/${route.activeIconPath}.svg` : `/images/etc/${route.iconPath}.svg`}
-                      width={16}
-                      height={16}
-                      style={{ marginRight: '8px' }}
-                      alt="route_path"
-                    />
-                    <Link href={route.path}>
-                      <span className={path === route.path ? 'choice' : 'common'} onClick={handleClickOther}>
-                        {route.label}
-                      </span>
+        <MenuContainer>
+          {router.map((item) => {
+            const { subMenu } = item;
+            const isActive = path.includes(item.path);
+            const openSub = openMenu.includes(item.path);
+            const iconPath = `/images/etc/${isActive ? item.activeIconPath : item.iconPath}.svg`;
+            const arrowPath = `/images/arrow/${isActive ? (openSub ? 'arrow_up' : 'arrow_down') : openSub ? 'arrow_up' : 'arrow_down'}.svg`;
+
+            return (
+              <li
+                className={isActive ? 'menu active' : 'menu'}
+                key={item.path}
+                onClick={subMenu ? () => handleClickOpen(item.path) : () => handleClickMenu(item.path)}
+              >
+                <MenuWrap>
+                  <Image src={iconPath} alt={item.label} width={16} height={16} style={{ marginRight: '8px' }} />
+                  {subMenu ? (
+                    <>
+                      <span className="menuLabel">{item.label}</span>
+                      <Image src={arrowPath} alt={'arrow'} width={16} height={16} />
+                    </>
+                  ) : (
+                    <Link href={'/admin' + item.path}>
+                      <span>{item.label}</span>
                     </Link>
-                  </>
-                )}
-
-                <div className={(route.subMenu && arcodian && activeMenu === route.path ? 'subMenu-wrap' : 'subMenu-hidden') || ''}>
-                  {route.subMenu && (
-                    <div className="subMenu-title">
-                      <Image
-                        src={activeMenu === route.path && arcodian === true ? `/images/etc/${route.activeIconPath}.svg` : `/images/etc/${route.iconPath}.svg`}
-                        width={16}
-                        height={16}
-                        alt="route_path"
-                        style={{ marginRight: '8px' }}
-                      />
-                      <span className={activeMenu === route.path ? 'choice' : 'common'} onClick={() => handleClick(route.path)}>
-                        {route.label}
-                      </span>
-                    </div>
                   )}
-                  <div className="subMenu">
-                    {arcodian &&
-                      activeMenu === route.path &&
-                      route.subMenu?.map((item) => (
-                        <Link href={item.path} key={item.path}>
-                          <span className={(path === item.path && 'choice') || ''}>{item.label}</span>
+                </MenuWrap>
+
+                {subMenu && (
+                  <StyledMenu height={item.height} openSub={openSub}>
+                    {subMenu?.map((subMenu) => {
+                      const isSubActive = path.includes(subMenu.path);
+
+                      return (
+                        <Link href={'/admin' + item.path + subMenu.path} key={subMenu.path}>
+                          <li className={isSubActive ? 'active' : ''}>{subMenu.label}</li>
                         </Link>
-                      ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </RouteWrapper>
+                      );
+                    })}
+                  </StyledMenu>
+                )}
+              </li>
+            );
+          })}
+        </MenuContainer>
       </div>
-      <div className="bottom"></div>
-    </SidebarContainer>
+      <div className="logout">
+        <Image src={'/images/etc/on_green.svg'} alt="logout" width={16} height={16} />
+        <span>로그아웃</span>
+      </div>
+      <Effect />
+    </Container>
   );
 };
 
-export default Sidebar;
+export default AdminSidebar;
 
-const SidebarContainer = styled.div`
+const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+
   display: flex;
   flex-direction: column;
-
-  width: 264px;
+  justify-content: space-between;
+  min-width: 264px;
   height: 95vh;
-  flex-shrink: 0;
+  box-sizing: border-box;
+  padding: 60px 41px 30px 41px;
+  margin: 20px 40px 20px 20px;
+
   border-radius: 16px;
   border: 1px solid var(--Stroke, rgba(255, 255, 255, 0.67));
   background: var(--Fill, linear-gradient(144deg, rgba(255, 255, 255, 0.1) 0%, rgba(0, 0, 0, 0) 100%), rgba(2, 6, 13, 0.5));
 
-  margin: 20px 40px 20px 20px;
+  .wrap {
+    flex: 1;
+    z-index: 1;
+  }
 
   .logo {
-    margin-left: 64px;
-    margin-top: 60px;
-    margin-bottom: 61px;
+    margin-left: 23px;
   }
-  .label {
-    color: var(--GRAY-WHITE, #fff);
-    font-family: 'Noto Sans KR';
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    margin-left: 16px;
 
+  .logout {
+    display: flex;
+    align-items: center;
+    margin-left: 36px;
     cursor: pointer;
+
+    span {
+      color: ${color.brand.brandMain};
+      font-size: 16px;
+      font-weight: 400;
+      margin-left: 16px;
+    }
   }
 `;
 
-const RouteWrapper = styled.div`
+const MenuContainer = styled.ul`
   /* position: fixed;
   top: 190px; */
   display: flex;
@@ -164,119 +184,60 @@ const RouteWrapper = styled.div`
 
   gap: 30px;
 
-  .choice {
-    color: ${color.brand.brandMain};
-    font-size: 16px;
-    font-weight: 700;
-  }
+  margin-top: 61px;
 
-  .common {
+  .menu {
     color: ${color.gray.white};
     font-size: 16px;
     font-weight: 400;
-  }
-
-  .route_box {
-    width: 214px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 8px;
-    /* color: #0066ff; */
-    background: #e2f1ff;
-    border-radius: 12px;
-
-    .wrap {
-      width: 150px;
-      display: flex;
-      align-items: center;
-    }
-    .arrow_icon {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      flex: 1;
-    }
-  }
-
-  .route_box_nosub {
-    width: 214px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 8px;
-    /* color: #0066ff; */
-
-    .wrap {
-      width: 150px;
-      display: flex;
-      align-items: center;
-    }
-    .arrow_icon {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      flex: 1;
-    }
-  }
-  .subMenu-wrap {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 40px;
-
-    width: 200px;
-    padding: 20px 0;
-
-    transition: 0.5s;
-  }
-
-  .subMenu-hidden {
-    transition: 0.5s;
-  }
-
-  .subMenu-title {
-    display: flex;
-    /* justify-content: center; */
-
-    align-items: center;
     cursor: pointer;
   }
 
-  .subMenu {
-    display: flex;
-    flex-direction: column;
-    justify-content: right;
-    gap: 40px;
-    color: black;
-
-    .choice {
-      color: #06f;
-    }
-
-    a {
-      display: flex;
-      justify-content: center;
-    }
+  .active {
+    color: ${color.brand.brandMain};
+    font-weight: 700;
   }
 `;
 
-const ProfileWrapper = styled.div`
+const MenuWrap = styled.div`
+  display: flex;
+  align-items: center;
+
+  .menuLabel {
+    flex: 1;
+  }
+`;
+
+const StyledMenu = styled.ul<{ openSub: boolean; height: number }>`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  gap: 10px;
+  justify-content: flex-end;
+  gap: 20px;
 
-  span {
-    /* color: #0066ff; */
+  height: ${({ height, openSub }) => (openSub ? height + 13 : 0)}px;
+  overflow: hidden;
+  transition: height 0.2s ease-in;
+  transition-property: display 0.2s ease-in;
+
+  li {
+    color: ${color.gray.gray5};
     font-size: 14px;
-  }
+    font-weight: 400;
 
-  .name {
-    font-size: 16px;
+    box-sizing: border-box;
+    padding-left: 24px;
   }
+`;
 
-  button {
-    margin-top: 10px;
-  }
+const Effect = styled.div`
+  position: absolute;
+  top: 30px;
+  left: -220px;
+
+  width: 405.797px;
+  height: 268.688px;
+  transform: rotate(-30deg);
+  flex-shrink: 0;
+
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.15) 0%, rgba(0, 0, 0, 0) 100%);
 `;
