419cbebf46dc8d734d3df18cf24a39c173827988
Jiwon <hajw.study@gmail.com>
2024-03-08 12:05:23 +0900

    admin 보증금 심사 api

--- src/app/admin/projectManage/depositJudge/page.tsx
+++ src/app/admin/projectManage/depositJudge/page.tsx
@@ -8,28 +8,33 @@ import SearchBar from '@/components/SearchBar/SearchBar';
 import SelectBox from '@/components/SelectBox/SelectBox';
 import Input from '@/components/Input/Input';
 import { rest } from '@/api/rest';
-import { getDepositList } from '@/api/admin/requireJudege/api';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import { DEPOSIT_LIST_REQUEST } from '@/api/admin/requireJudege/model';
 import PaginationComponent from '@/components/Pagination/Pagination';
+import useChangeInput from '@/hook/useChangeInput';
+import useChangeSelect from '@/hook/useChangeSelect';
+import { PROJECT_REQUIRE_JUDGE_OPTION } from 'public/static/requireJudge/static';
+import { getDepositJudgeList } from '@/api/admin/depositJudge/api';
+import { DEPOSIT_JUDGE_LIST, DEPOSIT_JUDGE_LIST_REQUEST } from '@/api/admin/depositJudge/model';
 
 const initialParams = {
   page: 1,
-  perPage: 50,
-  status: 33,
+  perPage: 20,
+  status: null,
   name: '',
-  nickname: '',
 };
 
 const Page = () => {
   const [params, setParams] = useState(initialParams);
+  const { input, onChange, setInput } = useChangeInput(initialParams.name);
+  const { select, onChange: onChangeSelect, setSelect } = useChangeSelect(initialParams.status);
+
   const { data } = useQuery({
-    queryKey: [rest.get.depositList, params],
-    queryFn: getDepositList,
+    queryKey: [rest.get.depositJudgeList, params],
+    queryFn: ({ queryKey }) => getDepositJudgeList(queryKey[1] as DEPOSIT_JUDGE_LIST_REQUEST),
   });
 
-  const column: ColumnDef<DEPOSIT_LIST_REQUEST>[] = [
+  const column: ColumnDef<DEPOSIT_JUDGE_LIST>[] = [
     {
       accessorKey: 'id',
       header: 'No.',
@@ -44,14 +49,28 @@ const Page = () => {
     },
   ];
 
-  const handleClickSearch = () => {};
-
   const handleClickReset = () => {
-    setParams(initialParams);
+    setParams({
+      ...initialParams,
+      page: params.page,
+    });
+    setInput(initialParams.name);
+    setSelect(initialParams.status);
   };
 
-  const onChange = (e: number) => {
-    console.log("e", e)
+  const handleClickSearch = () => {
+    setParams({
+      ...params,
+      name: input,
+      status: select,
+    });
+  };
+
+  const onChangePage = (e: number) => {
+    setParams({
+      ...params,
+      page: e,
+    });
   };
 
   return (
@@ -61,27 +80,30 @@ const Page = () => {
         <div className="content">
           <label>검색어</label>
           <div className="item">
-            <SelectBox options={[{ label: '프로젝트명', value: 1 }]} />
-            <Input size="small" style={{ width: 434 }} />
+            <SelectBox onChange={onChangeSelect} style={{ width: 140 }} options={[{ label: '프로젝트명', value: 1 }]} value={1} />
+            <Input onChange={onChange} value={input} size="small" style={{ width: 434 }} />
           </div>
         </div>
 
         <div className="content">
           <label>심사현황</label>
           <div className="item">
-            <SelectBox options={[{ label: '프로젝트명', value: 1 }]} />
-          </div>
-        </div>
-
-        <div className="content">
-          <label>보증금 지급 현황</label>
-          <div className="item">
-            <SelectBox options={[{ label: '프로젝트명', value: 1 }]} />
+            <SelectBox name="status" onChange={onChangeSelect} style={{ width: 140 }} options={PROJECT_REQUIRE_JUDGE_OPTION} value={select} />
           </div>
         </div>
       </SearchBar>
-      <Table columns={column} data={data || []} />
-      <PaginationComponent activePage={1} itemsCountPerPage={50} totalItemsCount={27} pageRangeDisplayed={10} onChange={onChange} />
+
+      <div className="tableWrapper">
+        <span className="totalCount">검색결과: {data?.dataList?.length}</span>
+        <Table columns={column} data={data?.dataList || []} />
+        <PaginationComponent
+          activePage={params.page}
+          itemsCountPerPage={20}
+          totalItemsCount={data?.totalCount || 0}
+          pageRangeDisplayed={10}
+          onChange={onChangePage}
+        />
+      </div>
     </Container>
   );
 };
@@ -94,5 +116,17 @@ const Container = styled.div`
     color: ${color.brand.brandMain};
     font-size: 24px;
     font-weight: 700;
+  }
+
+  .tableWrapper {
+    display: flex;
+    gap: 2px;
+    flex-direction: column;
+    margin-top: 32px;
+
+    .totalCount {
+      color: ${color.gray.gray6};
+      font-size: 12px;
+    }
   }
 `;
