bd3ba2e6fcf377d6bac99a17fdbcb1f5d115491e
hanamDeveloper <zmf1118@gmail.com>
2024-03-14 21:27:15 +0900

    모집글 작성 url 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Button from '../Button/Button';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Header = () => {
   const route = useRouter();
@@ -16,17 +17,16 @@ const Header = () => {
     setModal(false);
   };
 
-  const handleClickCreatePost = () => {
-    route.push('/project');
-  };
   return (
     <StyledHeader>
       <MyPageContainer visible={modal} onClose={handleCloseModal} />
       <Image src="/images/Logo.svg" alt="로고" className="logo" width={167} height={58} onClick={() => route.push('/')} style={{ cursor: 'pointer' }} />
       <div className="profile-wrap">
-        <Button leftIcon="/images/etc/editor_black.svg" size="medium" variant="primary" onClick={handleClickCreatePost}>
-          모집글 작성
-        </Button>
+        <Link href="post">
+          <Button leftIcon="/images/etc/editor_black.svg" size="medium" variant="primary">
+            모집글 작성
+          </Button>
+        </Link>
         <Profile />
         <Image src="/images/icons/Person_white.svg" alt="myPage" width={24} height={24} onClick={() => setModal(true)} style={{ cursor: 'pointer' }} />
         <Image src="/images/icons/On_white.svg" alt="logout" width={24} height={24} style={{ cursor: 'pointer' }} />
