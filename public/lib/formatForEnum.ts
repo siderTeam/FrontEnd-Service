7a6fbafc0a50aea6f9a15c0132a8a0cf2d5cafa2
MyeonghwaSon <myeonghwa0116@gmail.com>
2024-03-22 16:11:46 +0900

    formatForEnum>프로젝트 상태 & 요구사항 심사 진행 상태 합친 formatForProjectAllStatus 추가

--- public/lib/formatForEnum.ts
+++ public/lib/formatForEnum.ts
@@ -1,4 +1,4 @@
-import { POSITION_CODE, PROJECT_REQUIRE_JOIN_STATUS, PROJECT_STATUS } from './enum';
+import { POSITION_CODE, PROJECT_REQUIRE_JOIN_STATUS, PROJECT_REQUIRE_JUDGE_PROGRESS_STATUS, PROJECT_STATUS } from './enum';
 
 /**
  * [프로젝트 상태]
@@ -67,6 +67,46 @@ export const formatForProjectJoinStatus = (status: PROJECT_REQUIRE_JOIN_STATUS):
     [PROJECT_REQUIRE_JOIN_STATUS.REJECTED, '반려'],
     [PROJECT_REQUIRE_JOIN_STATUS.APPROVED, '승인'],
     [PROJECT_REQUIRE_JOIN_STATUS.CANCELED, '취소'],
+  ]);
+
+  return returnStatus.get(status) ?? '';
+};
+
+/**
+ * [프로젝트 상태]
+ * 모집중 19,
+ * 모집완료 20,
+ * 입금 대기중 22,
+ * 진행 대기 23,
+ * 진행중 24,
+ * 중도 종결 25,
+ * 준공 심사중 26,
+ * 제출완료 27,
+ * 준공 심사완료 28,
+ * 입금 기간 초과 41,
+ * 프로젝트 종료 21,
+ * 
+ * [프로젝트 요구사항 심사 진행 상태]
+ * 요구사항 정의 중 32,
+ * 요구사항 심사중 33,
+ * 요구사항 심사 반려 34,
+ * 요구사항 심사 승인 / 보증금 입금 대기 35,
+ */
+export const formatForProjectAllStatus = (status: PROJECT_STATUS | PROJECT_REQUIRE_JUDGE_PROGRESS_STATUS): string => {
+  const returnStatus = new Map<number, string>([
+    [PROJECT_STATUS.RECRUITING, '모집중'],
+    [PROJECT_STATUS.RECRUITMENT_COMPLETED, '모집완료'],
+    [PROJECT_STATUS.DEPOSIT_WAITING, '입금 대기중'],
+    [PROJECT_STATUS.WAITING_TO_START, '진행 대기'],
+    [PROJECT_STATUS.IN_PROGRESS, '진행중'],
+    [PROJECT_STATUS.PARTIAL_TERMINATION, '중도 종결'],
+    [PROJECT_STATUS.ASSESSMENT_IN_PROGRESS, '준공 심사중'],
+    [PROJECT_STATUS.SUBMISSION_COMPLETED, '제출완료'],
+    [PROJECT_STATUS.ASSESSMENT_COMPLETED, '준공 심사완료'],
+    [PROJECT_STATUS.DEPOSIT_PERIOD_EXPIRED, '입금 기간 초과'],
+    [PROJECT_STATUS.PROJECT_END, '프로젝트 종료'],
+    [PROJECT_REQUIRE_JUDGE_PROGRESS_STATUS.WAITING, '요구사항 심사중'],
+    [PROJECT_REQUIRE_JUDGE_PROGRESS_STATUS.REJECTED, '요구사항 심사 반려'],
   ]);
 
   return returnStatus.get(status) ?? '';
